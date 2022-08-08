ec9744e02d442c395de64a21a61438faf6f551a6
Shivanshu Pandey <shivanshipandey81@gmail.com>
2022-08-08 16:37:46 +0530

    assign

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mylodash = require('lodash')
 const abc = require('../introduction/intro')
 const First = require('../logger/logger');
 const Second = require('../util/helper')
@@ -12,6 +13,18 @@ router.get('/test-me', function (req, res) {
     First.Welcome()
     Second.printDate()
     third.Case()
+    let months = ["January", "February", "March", "April","May","June","July","August","September","October","November","December"]
+    console.log(mylodash.chunk(months,3))
+    let odd = [1,3,5,7,9,11,13,15,17,19]
+    console.log(mylodash.tail(odd))
+    let arr1 = ["hii"]
+    let arr2 = ["hello"]
+    let arr3 = ["hey"]
+    let arr4 = ["hlw"]
+    let arr5 = ["whatsupp"]
+    console.log(mylodash.union(arr1,arr2,arr3,arr4,arr5))
+    let arr = [["horror","The Shining"],["drama","titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+    console.log(mylodash.fromPairs(arr))
 });
 
 
