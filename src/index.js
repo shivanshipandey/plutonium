23bd88976c80a3a522829c0cb88764070fa95277
Shivanshu Pandey <shivanshipandey81@gmail.com>
2022-08-13 00:30:10 +0530

    just-kidding

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const { default: mongoose } = require('mongoose');
+
 const route = require('./routes/route.js');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', route);
+
+// mongoose.connect("mongodb+srv://shivanshi_pandey:3pFEn76pZirRNMou@cluster0.97ded1q.mongodb.net/?retryWrites=true&w=majority", {
+//     useNewUrlParser: true
+// })
+// .then( () => console.log("Mongodb is connected"))
+// .catch ( err => console.log(err))
+
+// app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
--- src/routes/route.js
+++ src/routes/route.js
@@ -17,8 +17,6 @@ router.get("/test-api" , function(req, res) {
 })
 
 
-
-
 router.get("/test-api-2" , function(req, res) {
     res.send("hi FunctionUp. This is another cool API")
 })
@@ -72,7 +70,6 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
-
 let players =
    [
        {
@@ -105,25 +102,101 @@ let players =
    ]
 
 
-   router.post ('/players', function (req,res){
-    let newMember = req.body
-    let memberName = newMember.name
-    let isNameRepeated = false 
-//i=0
-    for(let i = 0; i< players.length; i++){
-        if (players[i].name === memberName){
-            isNameRepeated == true;
-            break;
-        }
-    }
-
-    if(isNameRepeated){
-        res.send("This player was already added!")
-        }else{
-            players.push(newMember)
-            res.send(players)
-        }
-    })
+   router.post('/players',function(req,res) {
+        
+       let myNewPlayer=req.body
+       let myNewPlayerName= myNewPlayer.name
+       let isNameRepeated=false
    
+       for(i=0;i<players.length;i++) {
+           if(players[i].name == myNewPlayerName ){
+                isNameRepeated = true
+                break;
+           }
+    }
+    
+       if(isNameRepeated ){
+        res.send("MAN!, this player is already there") 
+
+
+       }else{
+        players.push(myNewPlayer)
+        res.send(players)
+       }
+
+   })
+
+
+
+
+   let arr = [ 23,43,54,65,6,7,87,98,45657,3456,655734,6,988]
+   router.post('/post-query2', function(req,res){
+
+
+    res.send({data: finalarr,status:true })
+
+   })
+
+
+   
+let persons= [
+  {
+  name: "ADESH",
+  age: 20,
+  votingStatus: false
+},
+{
+  name: "ROSHNI",
+  age: 25,
+  votingStatus: false
+},
+{
+  name: "JYOTI",
+  age: 23,
+  votingStatus: false
+},
+{
+  name: "PIYUSH",
+  age: 25,
+  votingStatus: false
+},
+{
+    name: "SHIVANSHI",
+    age: 22,
+    votingStatus: false
+},
+{
+    name : "GOL.D.ROGER",
+    age: 80,
+    votingStatus: false
+},
+{
+  name: "NIKHIL",
+  age: 40,
+  votingStatus: false
+}
+]
+
+router.post('/voters', function(req,res) {
+
+    let param = req.query;
+    let age = param.age;
+    let voterArray = [];
+    
+
+   for(let i =0; i < persons.length ; i++){
+      if(persons[i].age >= age){
+           persons[i].votingStatus = true
+          voterArray.push(persons[i])  
+      }
+   }
+
+  res.send(voterArray)
+          
+})
+
+
+
+
 
 module.exports = router;
