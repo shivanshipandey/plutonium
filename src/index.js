f0f4b6b0543b1b137f071013577359a1c0334db1
shivanshipandey <shivanshipandey81@gmail.com>
2022-08-26 17:52:18 +0530

    heloo

--- src/index.js
+++ src/index.js
@@ -13,13 +13,18 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
-
-app.use (
+//helooooooooo
+app.use(
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        console.log("inside GLOBAL MW");
+        const moment = require('moment');
+        const today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+        let ipAddress = req.ip;
+        let url = req.originalUrl
+        console.log (today +" , "+ipAddress+" , "+url);
         next();
-  }
-  );
+    }
+);
 
 app.use('/', route);
 
