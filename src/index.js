435e2f042f885d33f83395b634dcd8044157e7b0
shivanshipandey <shivanshipandey81@gmail.com>
2022-08-30 18:35:55 +0530

    authorisation

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Shivanshi:zCsGYxnp3CGNrB4b@cluster0.97ded1q.mongodb.net/Shivanshi0001=DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
